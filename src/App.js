630a4cb67aa50880a9cb829ac11790e5d31652e4
Liron <liron.br@bw-robotics.com>
2022-07-02 20:50:22 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './lightyear.jpeg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-container">
+      <div className="buzz-card">
+        <h1>lightyear</h1>
+        <img src={logo} alt="My little unicorn" />
+        <p>Buzz Lightyear is a fictional character in the Toy Story franchise created by Disney and Pixar mainly voiced by Tim Allen.He is a sentient toy action figure based on the in-universe film and television franchise Buzz Lightyear of Star Command, a "Space Ranger" superhero.</p>
+        <a href="https://en.wikipedia.org/wiki/Buzz_Lightyear">Learn More</a>
+      </div>
     </div>
   );
 }
